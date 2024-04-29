1e9256e5b8ec66f2585f4e7d6f8d5d94621763bc
YuongPopai <149505686+YuongPopai@users.noreply.github.com>
2024-04-29 12:55:23 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+base_url = 'https://pay-test.raif.ru/api/sbp/v2/qrs/'
+secret_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU'
+headers = {'Authorization': f'Bearer {secret_key}'}
+sbpMerchantId = 'MA622976'
+user_file_path = 'users.csv'
+admin_file_path = 'admins.csv'
+bot_key = '7144770165:AAHc5K6nj7yZW4_D0tyIuBjyi9eEWDVpuws'
